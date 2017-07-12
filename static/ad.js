e9160cd9a86d0d131d4d08f24643ca8f86a783e0
mnhkahn <lichao@cyeam.com>
2017-07-12 09:56:51 +0800

    add ad

---
+++ static/ad.js
@@ -0,0 +1,7 @@
+var jd_union_unid = "1000081354",
+    jd_ad_ids = "507:6",
+    jd_union_pid = "CL3x5obTKxDKj/DcAxoAIJm78rMDKgA=";
+var jd_width = 728;
+var jd_height = 90;
+var jd_union_euid = "";
+var p = "BhcCVhhZEQIaNwpfBkgyTUMIRmtKRk9aZV8ETVxNNwpfBkgya3wAAT5cXXVkUWE8HUlrfgBsPBNVRAtZK14RBhEEVBhYFTISBlQaWhwCEANQK2tKRk9aZVA1FDJNQwhGaxUHGg9THFkVBRIEUxJrFDIiNw%3D%3D";
