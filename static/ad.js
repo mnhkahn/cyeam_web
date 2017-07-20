b77a5f0e08924033ce3fc7ba6a5e69efe79b5de9
mnhkahn <lichao@cyeam.com>
2017-07-20 10:33:19 +0800

    兼容移动和客户端

--- static/ad.js
+++ static/ad.js
@@ -1,2 +1,15 @@
-var jd_union_pid="926789314";
-var jd_union_euid="";
+var ua = navigator.userAgent;
+var isAndroid = /Android/i.test(ua);
+var isBlackBerry = /BlackBerry/i.test(ua);
+var isWindowPhone = /IEMobile/i.test(ua);
+var isIOS = /iPhone|iPad|iPod/i.test(ua);
+var isMobile = isAndroid || isBlackBerry || isWindowPhone || isIOS;
+
+var jd_union_pid = "926796553";
+var jd_union_euid = "";
+
+if (isMobile) {
+    // 小米手环2抢券9块9
+    var jd_union_pid = "926767564";
+    var jd_union_euid = "";
+}
